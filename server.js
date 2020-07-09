59754ac7dc15407a39208a83d1381dbf663baa3d
Rakil Ahmed <imrakilahmed@gmail.com>
2020-07-08 22:36:14 -0400

    env variable chnaged

--- server.js
+++ server.js
@@ -2,12 +2,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+require('dotenv').config();
 
 // App + PORT + Database URL
+
 const app = express();
 const PORT = process.env.PORT || 3000;
-const CONNECTION_URI =
-  process.env.MONGODB_URI || 'mongodb://localhost:27017/covid';
+const CONNECTION_URI = process.env.MONGODB_URI || process.env.DB_URI;
 
 // Routes
 const covidDataRoute = require('./routes/data-routes');
