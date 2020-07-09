15ef2b5cccac586e6dcb40dc13d3bfa0a54d938e
Rakil Ahmed <imrakilahmed@gmail.com>
2020-07-08 21:50:38 -0400

    fixed router

--- routes/data-routes.js
+++ routes/data-routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Data = require('../models/Data');
 
-app.get('/', async (req, res) => {
+router.get('/', async (req, res) => {
   return res.redirect('/api');
 });
 
