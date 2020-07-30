ee1946239da6667a8663b0b52bcc0b74827aa142
Rakil Ahmed <imrakilahmed@gmail.com>
2020-07-30 19:40:01 -0400

    minor

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const CONNECTION_URI = process.env.MONGODB_URI || process.env.DB_URI;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 
 const covidRoutes = require('./routes/data-routes');
 app.use(covidRoutes);
